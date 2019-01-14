aaef44b127dedd9bcd61c4cabb2a799b2eecdb66
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-01-14 09:05:35 -0500

    Use shared module in information module

--- projects/iv-widget/src/app/steps/information/information.module.ts
+++ projects/iv-widget/src/app/steps/information/information.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InformationComponent } from './information.component';
-import { SharedModule } from '../../shared/shared.module';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TrustedHtmlModule } from '../../pipes/trusted-html/trusted-html.module';
-import { ThemeModule } from '../../theme/theme.module';
+
+import { SharedModule } from '../../shared/shared.module';
+import { InformationComponent } from './information.component';
 
 @NgModule({
   declarations: [InformationComponent],
