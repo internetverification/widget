290a4158ecee9a610d230e870bc137c0d8d15584
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-01-29 10:07:54 -0500

    Add circleci config and update e2e tests

--- projects/iv-widget-e2e/src/app.e2e-spec.ts
+++ projects/iv-widget-e2e/src/app.e2e-spec.ts
@@ -1,14 +1,36 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
+    browser.waitForAngularEnabled(false);
     page = new AppPage();
+    page.navigateTo();
+    browser.driver.sleep(1000);
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to iv-widget!');
+  it('Should be able to switch lang', async () => {
+    const defaultTitle = await page.getStepTitle();
+    page.switchLang();
+    const changedTitle = await page.getStepTitle();
+    expect(defaultTitle).not.toBe(changedTitle);
+  });
+
+  it('Should be able to switch theme', async () => {
+    const defaultTheme = await page.getWidgetThemeClass();
+    page.switchTheme();
+    const changedTheme = await page.getWidgetThemeClass();
+    expect(defaultTheme).not.toBe(changedTheme);
+  });
+
+  it('Default scenario should run', async () => {
+    page.clickNext('I Agree');
+    page.clickNext('Take selfie picture');
+    page.clickNext('Upload photo');
+    page.clickNext('Take Id picture');
+    page.clickNext('Upload photo');
+    expect(await page.getSummaryStepCount()).toBe(2);
   });
 });
--- projects/iv-widget-e2e/src/app.po.ts
+++ projects/iv-widget-e2e/src/app.po.ts
@@ -5,7 +5,45 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getTitleText() {
-    return element(by.css('ivw-root h1')).getText();
+  getStepTitle() {
+    return element(by.css('iv-widget h1')).getText();
+  }
+
+  getWidgetThemeClass() {
+    return element(by.css('iv-widget div')).getAttribute('class');
+  }
+
+  getSummaryStepCount() {
+    return element.all(by.css('ivw-summary-step')).count();
+  }
+
+  clickNext(text) {
+    const returnedValue = element(by.buttonText(text)).click();
+    browser.driver.sleep(1000);
+    return returnedValue;
+  }
+
+  async switchLang() {
+    const selectDropdownbyNum = function(select, optionNum) {
+      if (optionNum) {
+        select.all(by.tagName('option')).then(function(options) {
+          options[optionNum].click();
+        });
+      }
+    };
+    const el = element(by.id('lang'));
+    selectDropdownbyNum(el, 1);
+  }
+
+  async switchTheme() {
+    const selectDropdownbyNum = function(select, optionNum) {
+      if (optionNum) {
+        select.all(by.tagName('option')).then(function(options) {
+          options[optionNum].click();
+        });
+      }
+    };
+    const el = element(by.id('theme'));
+    selectDropdownbyNum(el, 1);
   }
 }
